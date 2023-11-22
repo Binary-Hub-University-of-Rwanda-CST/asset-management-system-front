e0058863c7253df1576a89f9446b7f508268e689
code with edison <edsnkvn@gmail.com>
2023-11-22 16:49:58 +0200

    Project setup with ReactJs, Typescript, Redux, Tailwindcss, Axios, React Icons

---
+++ asset-management-system/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+
+  // this is sample code for test tailwind css
+  // you can delete it while you start development 
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className='font-bold text-[#2d90d2]' >
+        <h1 className='text-center text-2xl'>UNIVERSITY OF RWANDA</h1>
+        <h1 className='text-center text-xl'> ASSET MANAGEGEMENT SYSTEM </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
