7d997b7580af9592905ea2d844599d2edd0104f8
patienceIneza <inezapatience2@gmail.com>
2023-11-26 11:01:23 +0200

    File Upload and switch input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import FileUploadPreview from './components/FileUpload';
+import ToggleSwitch from './components/Switch';
+
 
 function App() {
 
@@ -10,6 +13,8 @@ function App() {
       <div className='font-bold text-[#2d90d2]' >
         <h1 className='text-center text-2xl'>UNIVERSITY OF RWANDA</h1>
         <h1 className='text-center text-xl border-l '> ASSET MANAGEMENT SYSTEM </h1>
+        <FileUploadPreview/>
+        <ToggleSwitch/>
       </div>
     </div>
     </>
