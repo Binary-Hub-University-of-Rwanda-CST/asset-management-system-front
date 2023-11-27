29ab1467f543bf3f1eca9003d14a432ca9e06f4d
Mayor vargas <uwihanganyeobed@gmail.com>
2023-11-27 17:52:56 +0200

    This is my branch

---
+++ src/Alerts & notifications pushed/variants.jsx
@@ -0,0 +1,43 @@
+export const variants = [
+   // red
+   {
+     mainColor: "#FDEDED",
+     secondaryColor: "#F16360",
+     symbol: "Delete",
+     title: "Error",
+     text: "Attempted error!! Please try again.",
+   },
+   // blue
+   {
+     mainColor: "#E5F6FD",
+     secondaryColor: "#1AB1F5",
+     symbol: "Log",
+     title: "Information",
+     text: "Sign-in as Admin or as guest ",
+   },
+   // green
+   {
+     mainColor: "#EDFEEE",
+     secondaryColor: "#5CB660",
+     symbol: "Save",
+     title: "Success",
+     text: "Save assets in the stock",
+   },
+   // yellow
+   {
+     mainColor: "#FFF4E5",
+     secondaryColor: "#FFA117",
+     symbol: "Warn",
+     title: "Warning",
+     text: "Saving, please wait",
+   },
+   // pink
+   {
+     mainColor: "#FFC0CB",
+     secondaryColor: "#FF69B4",
+     symbol: "Good",
+     title: "Feedback",
+     text: " Congratulations stay tuned to binary Hub",
+   },
+ ];
+ 
