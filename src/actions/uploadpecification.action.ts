76c805005520e41e598a2f7a817923e3420dd084
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-07-13 12:14:47 +0200

    -upload specificaiton actions

--- src/actions/uploadpecification.action.ts
+++ src/actions/uploadpecification.action.ts
@@ -2,19 +2,30 @@
 
 import { Dispatch } from 'redux';
 import { API_URL } from '../utils/api';
-import { AssetSpecification } from '../utils/uploadSpecification';
+// import { AssetSpecification } from '../utils/uploadSpecification';
 import { FETCH_UPLOAD_SPECIFICATION_REQUEST, FETCH_UPLOAD_SPECIFICATION_FAILURE, FETCH_UPLOAD_SPECIFICATION_SUCCESS } from './types';
 
 // Local asset specifications
 import assetSpecifications from '../utils/uploadSpecification';// Adjust the path based on your file structure
 
 // Action creators
+
+export interface AssetSpecification {
+    name: string;
+    type: string;
+    required: boolean;
+    unique?: boolean;
+    allowedValues?: string[]; 
+
+
+}
+
 export const fetchSpecifications = () => {
     return async (dispatch: Dispatch) => {
         dispatch({ type: FETCH_UPLOAD_SPECIFICATION_REQUEST });
 
         try {
-            let data: AssetSpecification[] = [];
+            let data: AssetSpecification[] = []; 
 
             // Use local specifications first
             data = assetSpecifications;
@@ -23,11 +34,11 @@ export const fetchSpecifications = () => {
             await new Promise(resolve => setTimeout(resolve, 1000));
 
             // Optionally, fetch from API if needed
-            // const response = await fetch(`${API_URL}/specification/all`);
+            // const response = await fetch(`${API_URL}/asset/validationData`);
             // if (!response.ok) {
             //     throw new Error('Failed to fetch specifications');
             // }
-            // data = await response.json();
+            // data = await response.json();  
 
             dispatch({ type: FETCH_UPLOAD_SPECIFICATION_SUCCESS, payload: data });
         } catch (error) {
