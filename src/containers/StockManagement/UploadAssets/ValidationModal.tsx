4447e21c9769994b695da72b0ce7ac0fa0a3ca44
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-07-16 23:08:25 +0200

    final validation

--- src/containers/StockManagement/UploadAssets/ValidationModal.tsx
+++ src/containers/StockManagement/UploadAssets/ValidationModal.tsx
@@ -44,7 +44,26 @@ const ValidationModal: React.FC<ModalProps> = ({
   }, [tableData, uploadSpecification, tableHeaders]);
   const dispatch: AppDispatch = useDispatch();
   
+  // helper to hadle number 
+  const parseNumber = (value: string): number | null => {
+    if (typeof value === 'number') return value;
+    if (typeof value !== 'string') return null;
   
+    // Remove commas and percentage signs
+    let cleanedValue = value.replace(/,/g, '').replace(/%$/, '');
+  
+    // Try parsing the cleaned value
+    let parsedValue = parseFloat(cleanedValue);
+  
+    if (isNaN(parsedValue)) return null;
+  
+    // If it was a percentage, divide by 100
+    if (value.endsWith('%')) {
+      parsedValue /= 100;
+    }
+  
+    return parsedValue;
+  };
 
 
   const validateData = (data: Record<string, any>[]) => {
@@ -75,14 +94,20 @@ const ValidationModal: React.FC<ModalProps> = ({
             error = `Invalid value. Expected: ${allowedValuesMessage}`;
           } else if (spec.type === "string" && typeof cellValue !== "string") {
             error = "Expected a string value";
-          } else if (spec.type === "number" && isNaN(parseFloat(cellValue))) {
-            error = "Must be a number";
+          } else if (spec.type === "number" && cellValue !==""  && parseNumber(cellValue) === null) {
+            error = "Must be a valid number";
+          } else if (spec.type === "boolean" && !['true', 'false', true, false].includes(cellValue)) {
+            error = "Must be a boolean value";
+          } else if (spec.type === "date" && cellValue !==""  && isNaN(Date.parse(cellValue))) {
+            error = "Must be a valid date";
           } else if (spec.unique && uniqueValuesTracker[header]?.has(cellValue)) {
-            error = "Duplicate value found";
+            error = "Duplicate value found"; 
           }
   
           if (error) {
             hasErrors = true;
+          } else if (spec.unique) {
+            uniqueValuesTracker[header].add(cellValue);
           }
   
           validatedRow[header] = { value: cellValue, error };
@@ -124,18 +149,35 @@ const ValidationModal: React.FC<ModalProps> = ({
     }
   };
 
-  const transformDataForUpload = (data: any[]) => {
-    return data.map((row) => {
-      
-      const transformedRow: { [key: string]: any } = {};
-      for (const key in row) {
-        if (row.hasOwnProperty(key)) {
+const transformDataForUpload = (data: any[]) => {
+  return data.map((row) => {
+    const transformedRow: { [key: string]: any } = {};
+    for (const key in row) {
+      if (row.hasOwnProperty(key) && key !== 'hasErrors') {
+        const spec = uploadSpecification.find((s) => s.name === key);
+        if (spec) {
+          switch (spec.type) {
+            case 'number':
+              const parsedNumber = parseNumber(row[key].value);
+              transformedRow[key] = parsedNumber !== null ? parsedNumber : 0;
+              break;
+            case 'boolean':
+              transformedRow[key] = row[key].value === 'true' || row[key].value === true;
+              break;
+            case 'date':
+              transformedRow[key] = new Date(row[key].value).toISOString();
+              break;
+            default:
+              transformedRow[key] = row[key].value;
+          }
+        } else {
           transformedRow[key] = row[key].value;
         }
       }
-      return transformedRow;
-    });
-  };
+    }
+    return transformedRow;
+  });
+};
   
   
   const handleSaveData = () => {
@@ -153,7 +195,6 @@ const ValidationModal: React.FC<ModalProps> = ({
       setSaveError('Cannot save data. Please fix validation errors.');
     }
   };
-   
   
   
 
@@ -179,8 +220,12 @@ const ValidationModal: React.FC<ModalProps> = ({
         error = `Invalid value. Expected: ${allowedValuesMessage}`;
       } else if (spec.type === "string" && typeof newValue !== "string") {
         error = "Expected a string value";
-      } else if (spec.type === "number" && isNaN(parseFloat(newValue))) {
-        error = "Must be a number";
+      } else if (spec.type === "number" && parseNumber(newValue) === null) {
+        error = "Must be a valid number";
+      } else if (spec.type === "boolean" && !['true', 'false', true, false].includes(newValue)) {
+        error = "Must be a boolean value";
+      } else if (spec.type === "date" && isNaN(Date.parse(newValue))) {
+        error = "Must be a valid date";
       } else if (
         spec.unique &&
         updatedData.some(
@@ -205,7 +250,7 @@ const ValidationModal: React.FC<ModalProps> = ({
   
       setFilteredData(updatedData);
     }
-  };
+  }; 
    
 
   return (
@@ -293,25 +338,25 @@ const ValidationModal: React.FC<ModalProps> = ({
                               key={header}
                               className="px-4 py-3 text-left text-sm font-bold text-black uppercase"
                             >
-                              {header}
+                              {header.replace(/_/g, ' ')} 
                             </th>
                           ))}
                         </tr>
                       </thead>
                       <tbody>
                         {filteredData.map((row, rowIndex) => (
-                          <tr key={rowIndex}>
+                          <tr key={rowIndex} className={` `}>  
                             {showRemove && (
                               <td className="px-4 py-1 font-sm">
                                 <button
                                   className="px-4 py-1 rounded-md border hover:bg-danger border-red-500"
                                   onClick={() => handleDeleteRow(rowIndex)}
                                 >
-                                  <FaTrashAlt className="text-red-700" />
+                                  <FaTrashAlt className="text-red-700" /> 
                                 </button>
                               </td>
                             )}
-                            <td className="px-2 py-1 font-sm">
+                            <td className={`px-2 py-1 font-sm ${hasAnyErrors(row) ? 'bg-red-400 font-bold text-sm ': ''}`}>
                               {rowIndex + 1}
                             </td>
                             {tableHeaders.map((header, cellIndex) => {
@@ -340,6 +385,11 @@ const ValidationModal: React.FC<ModalProps> = ({
                                           )
                                         }  
                                       >
+                                        {row[header].error && (
+                                          <option value={row[header].value} className="bg-red-200  overline text-red-600">
+                                            {row[header].value} (Invalid)
+                                          </option>
+                                        )}
                                         <option value="" disabled>
                                           Select an option
                                         </option>
@@ -349,6 +399,7 @@ const ValidationModal: React.FC<ModalProps> = ({
                                           </option>
                                         ))}
                                       </select>
+ 
                                       {row[header].error && (
                                         <div className="text-red-600 text-sm mt-1">
                                           {row[header].error}
