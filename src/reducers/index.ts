dc4862b415f6c4ffe6bc9b4f4e20c7da7b6673de
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-07-22 15:25:07 +0200

    Modify src/reducers/index.ts

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,23 +3,29 @@ import { Auth, System } from "../actions";
 import { authReducer } from "./auth.reducer";
 import { systemReducer } from "./system.reducer";
 import assetReducer, { AssetState } from "./asset.reducer";
-  import specificationReducer, {SpecificationState} from "./specification.reducer";
-  import validatedDataReducer, {ValidatedDataState} from "./saveUpload.reducer";
+import specificationReducer, {
+  SpecificationState,
+} from "./specification.reducer";
+import validatedDataReducer, { ValidatedDataState } from "./saveUpload.reducer";
+import changePasswordReducer, {
+  ChangePasswrdState,
+} from "./changePassword.reducer";
 
 // define the entire state into the entire side
 export interface StoreState {
   auth: Auth;
   system: System;
-  asset:AssetState;
-  uploadSpecificaiton: SpecificationState
-  validatedData: ValidatedDataState
-  
+  asset: AssetState;
+  uploadSpecificaiton: SpecificationState;
+  validatedData: ValidatedDataState;
+  changePassword: ChangePasswrdState;
 }
 
 export const rootReducers = combineReducers<StoreState>({
   auth: authReducer,
   system: systemReducer,
   asset: assetReducer,
-  uploadSpecificaiton: specificationReducer, 
-  validatedData: validatedDataReducer 
-});
+  uploadSpecificaiton: specificationReducer,
+  validatedData: validatedDataReducer,
+  changePassword: changePasswordReducer,
+}); 
