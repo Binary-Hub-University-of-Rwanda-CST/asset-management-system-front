7766b8dc1f142eb45e1af05e23c3832eb203cf78
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-07-26 12:10:41 +0200

    Commit untracked directory: src/components/Footer/

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="pt-6 pb-2 w-full ">  
+      <div className="text-center font-light px-6 pb-2">
+        Asset Management System
+      </div>
+      <div className="bg-gray-100 text-center gap-2  w-full px-6 pb-4">
+        <a href='/meet-developers' target="_blank"    className=" text-sm font-bold underline text-my-blue ">Binary Hub @ copyright {new Date().getFullYear()}</a> 
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
